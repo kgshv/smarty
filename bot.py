4d0d80e5fc4caa6c5c1a0abda544905ee96cf662
Roman Kolgushev <kolgushev@gmail.com>
2016-02-25 17:47:28 +0200

    Fixed the "help" reply message

--- bot.py
+++ bot.py
@@ -12,9 +12,6 @@ def main():
 	bot = Bot()
 	bot.run()
 
-
-
-
 @respond_to("^hi|^hello|^hi there|^what's up", re.IGNORECASE)
 def hi(message):
 	responses = ['Hello','Hello there','Hi there','Sup','Hey']
@@ -23,17 +20,26 @@ def hi(message):
 
 @respond_to('^help', re.IGNORECASE)
 def help(message):
-
-		reply = 'The *report* syntax: \n\
->You can say things like `report top 10 yesterday` or `report top 20 today` or \
-`report top 5 from 10-11-2015 to 30-02-2016`. That is date-month-year.\n\
-The schematics is always the same: the `report` keyword, followed by `top 10` \
-and a date or a from-to construct. Number after top *needs to be below or equal to 20*.\n\
-*Types of reports:*\n\
->Now, if you want to get something else then a list of 10 top articles, \
-you can also say `report url` followed by the full URL of the page on '+COMPANY_NAME+' you need analyzed and the date construct.\n\
+	reply = 'Start your request with the *report* keyword.\n\
+The *report* syntax: \n\
+The *top* keyword: \n\
+```report top 7 from 2016-02-01 to today```\n\
+In this example above, `report`, `top`, `from` and `to` are all keywords. They need to be in this particular order.\n\
+This query will give you a list of top 7 pages from Feb 1 2016 until today.\n\
+*The `today` term simply means "up to now"\n\
+*The date is in YEAR-MONTH-DATE format with dashes.\n\
+Number after `top` *needs to be below or equal to 20*.\n\
+*All results are *arranged by the number of page views* (unique and non unique)\n\
+  \n\
+  \n\
+Again, the structure is `report` space `top` space *number* space `from` date (can also be today or yesterday) space `to` space date.\n\
+>You can also say things like `report top 10 yesterday` or `report top 20 today` or \
+`report top 5 from 2015-11-10 to 2016-01-01`.\n\
+The *url* keyword: \n\
+>Now, if you want to get something else then a list of top pages, \
+you can also say `report url` followed by the full URL of the page on '+COMPANY_NAME+' you need analyzed, plus the date or dates.\n\
 So, *for example:*\n\
-```report url '+EXAMPLE_URL+' from 28-11-2014 to today```\n\
+```report url '+EXAMPLE_URL+' from 2014-11-28 to today```\n\
 Will tell you all stats for the '+EXAMPLE_URL_TITLE+' page on '+COMPANY_NAME+' for the respective time period.'
 
 		message.reply(reply)
